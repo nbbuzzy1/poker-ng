96fc388fcb2de6bde3a26543dda18c5fca23ba58
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-10-05 16:13:15 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { deck } from './deck';
+import { table } from './raising-hands';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'Poker';
-}
+export class AppComponent {}
