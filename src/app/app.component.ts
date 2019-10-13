f1b87bac852de28475388be7cafd3e6b4088e405
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-10-13 17:36:53 -0400

    Finished inital refactor of app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,178 +10,83 @@ import { table } from './raising-hands';
 export class AppComponent implements OnInit {
   public url1: string = "";
   public url2: string = "";
-  public position: string = "";
+  public positionName: string = "";
   public correct: boolean;
   public wrong: boolean;
-  public count: number = 0;
+  public currentWinStreak: number = 0;
   public longestWinStreak: number = 0;
+  private completeHand1 = '';
+  private completeHand2 = '';
+  private position: number;
+  public flashTrue: boolean = false;
 
   ngOnInit() {
+    this.drawCards();
+  }
 
-    const setImageUrl = (cardOne, cardTwo) => {
-      this.url1 = `assets/images/${cardOne}`;
-      this.url2 = `assets/images/${cardTwo}`;
+  private drawCards = () => {
+    const cardOne = deck[Math.floor(Math.random() * 52)];
+    const updatedDeck = deck.filter((card) => card !== cardOne);
+    const cardTwo = updatedDeck[Math.floor(Math.random() * 51)];
+    this.setTablePosition();
+    this.setImageUrl(cardOne, cardTwo);
+    this.setCompleteHand(cardOne, cardTwo);
+  }
+
+  private setImageUrl(cardOne, cardTwo) {
+    this.url1 = `assets/images/${cardOne}`;
+    this.url2 = `assets/images/${cardTwo}`;
+  }
+
+  private setCompleteHand = (cardOne, cardTwo) => {
+    let suited = "";
+    let offsuit = "";
+    if (cardOne[1] === cardTwo[1]) {
+      suited = "s";
+    } else if (cardOne[0] === cardTwo[0]) {
+      offsuit ='';
+      suited = '';
+    } else if (cardOne[1] !== cardTwo[1]) {
+      offsuit = "o";
     }
+    this.completeHand1 = cardOne[0] + cardTwo[0] + suited + offsuit;
+    this.completeHand2 = cardTwo[0] + cardOne[0] + suited + offsuit;
+  }
 
-    const setCompleteHand = (cardOne, cardTwo) => {
-      let suited = "";
-      let offsuit = "";
-      let completeHand = "";
-      let completeHand2 = "";
-      if (cardOne[1] === cardTwo[1]) {
-        suited = "s";
-      } else if (cardOne[0] === cardTwo[0]) {
-        offsuit ='';
-        suited = '';
-      } else if (cardOne[1] !== cardTwo[1]) {
-        offsuit = "o";
+  private setTablePosition() {
+    this.position = Math.floor(Math.random() * 8);
+    const positionName = ['Under the Gun', 'Under the Gun +1', 'Under the Gun +2',
+                          'Lowjack', 'Highjack', 'Cutoff', 'Button', 'Small Blind'];
+    this.positionName = positionName[this.position];
+  }
+
+  public verifyRaise() {
+    if (table[this.position].includes(this.completeHand1) || table[this.position].includes(this.completeHand2)) {
+      this.currentWinStreak++;
+      this.flashTrue = true;
+      if (this.currentWinStreak > this.longestWinStreak) {
+        this.longestWinStreak = this.currentWinStreak;
       }
-      completeHand = cardOne[0] + cardTwo[0] + suited + offsuit;
-      completeHand2 = cardTwo[0] + cardOne[0] + suited + offsuit;
+    } else {
+      this.currentWinStreak = 0;
     }
-
-    const drawCards = () => {
-      const cardOne = deck[Math.floor(Math.random() * 52)];
-      const updatedDeck = deck.filter((card) => card !== cardOne);
-      const cardTwo = updatedDeck[Math.floor(Math.random() * 51)];
-      setImageUrl(cardOne, cardTwo);
-      setCompleteHand(cardOne, cardTwo);
-    }
-
-    const setTablePosition = () => {
-      const position = Math.floor(Math.random() * 8);
-      const positionName = ['Under the Gun', 'Under the Gun +1', 'Under the Gun +2', 'Lowjack', 'Highjack', 'Cutoff', 'Button', 'Small Blind'];
-      this.position = positionName[position]
-    }
-
-
-
-
-  //analyzing whether raise or fold was correct or wrong
-  let result = document.getElementById("displayResult");
-  // result.addEventListener('animationend', function() {
-  //   result.classList.remove('flashit');
-  // });
-  const correct = () => {
-    result.innerHTML = "Correct!";
-    result.classList.add('flashit');
-    count++;
-    if (count > longestStreak) {
-      longestStreak = count;
-    }
-    document.getElementById("winStreak").innerHTML = "Count: " + count;
-    document.getElementById("longestStreak").innerHTML = "Longest Win Streak: " + longestStreak;
-  }
-  const wrong = () => {
-    result.innerHTML = "Wrong!";
-    result.classList.add('flashit');
-    count = 0;
-    document.getElementById("winStreak").innerHTML = "";
+    this.drawCards();
   }
 
-  //user clicks raise button
-  let raiseButton = document.getElementById("raiseBTN")
-  raiseButton.addEventListener("click", function(){
-    if (table[position].includes(completeHand) || table[position].includes(completeHand2)) {
-      correct();
+  public verifyFold() {
+    if (!(table[this.position].includes(this.completeHand1) || table[this.position].includes(this.completeHand2))) {
+      this.currentWinStreak++;
+      this.flashTrue = true;
+      if (this.currentWinStreak > this.longestWinStreak) {
+        this.longestWinStreak = this.currentWinStreak;
+      }
     } else {
-      wrong();
+      this.currentWinStreak = 0;
     }
-    newGame();
-  });
+    this.drawCards();
+  }
 
-  //user clicks fold button
-  let foldButton = document.getElementById("foldBTN")
-  foldButton.addEventListener("click", function(){
-    if (!table[position].includes(completeHand) && !table[position].includes(completeHand2)) {
-      correct();
-    } else {
-      wrong();
-    }
-    newGame();
-  });
-
-  //start over
-  let startOver = document.getElementById("startOver")
-  startOver.addEventListener("click", function() {
-    count = 0;
-    longestStreak = 0;
-    document.getElementById("winStreak").innerHTML = "";
-    document.getElementById("displayResult").innerHTML = "";
-    document.getElementById("longestStreak").innerHTML = "";
-    newGame();
-  })
-
-  //new game
-  const newGame = () => {
-    let deck = [
-      'AS.jpg',
-      'AC.jpg',
-      'AD.jpg',
-      'AH.jpg',
-      'KS.jpg',
-      'KC.jpg',
-      'KD.jpg',
-      'KH.jpg',
-      'QS.jpg',
-      'QC.jpg',
-      'QD.jpg',
-      'QH.jpg',
-      'JS.jpg',
-      'JC.jpg',
-      'JD.jpg',
-      'JH.jpg',
-      'TS.jpg',
-      'TC.jpg',
-      'TD.jpg',
-      'TH.jpg',
-      '9S.jpg',
-      '9C.jpg',
-      '9D.jpg',
-      '9H.jpg',
-      '8S.jpg',
-      '8C.jpg',
-      '8D.jpg',
-      '8H.jpg',
-      '7S.jpg',
-      '7C.jpg',
-      '7D.jpg',
-      '7H.jpg',
-      '6S.jpg',
-      '6C.jpg',
-      '6D.jpg',
-      '6H.jpg',
-      '5S.jpg',
-      '5C.jpg',
-      '5D.jpg',
-      '5H.jpg',
-      '4S.jpg',
-      '4C.jpg',
-      '4D.jpg',
-      '4H.jpg',
-      '3S.jpg',
-      '3C.jpg',
-      '3D.jpg',
-      '3H.jpg',
-      '2S.jpg',
-      '2C.jpg',
-      '2D.jpg',
-      '2H.jpg'
-    ];
-    // cardOne = deck[Math.floor(Math.random() * 52)];
-    // deck.splice(deck.indexOf(cardOne), 1);
-    // cardTwo = deck[Math.floor(Math.random() * 51)];
-    // getCardOne();
-    // getCardTwo();
-    // suited = '';
-    // offsuit = '';
-    // completeHand = '';
-    // completeHand2 = '';
-    // cardComparison();
-    // completeHand = cardOne[0] + cardTwo[0] + suited + offsuit;
-    // completeHand2 = cardTwo[0] + cardOne[0] + suited + offsuit;
-    // position = Math.floor(Math.random() * 8);
-    // showPosition();
-  };
+  private onChoice() {
+    setTimeout(() => this.flashTrue = false, 3000)
   }
 }
